6e17c89c82618ae3c74cde0c50a2197f66704136
Tatiana1103 <tasia1033@gmail.com>
2018-02-22 20:35:41 +0200

    Change captcha/validator

--- captcha/validator.js
+++ captcha/validator.js
@@ -10,9 +10,27 @@ window.onload = function() {
 function validatePassword(){
   var password = document.getElementById("short_link");
       confirm_password = document.getElementById("data");
+	alert("len of first" + password.value.length);
+	alert("len of second" + confirm_password.value.length);
+	if(password.value.length !=confirm_password.value.length ) {
+		alert("The length is't the same");
+	}
+	
+	for (var i=0; i < password.value.length; i++) {
+		alert("i = " + i);
+		if (password.value.charAt(i) != confirm_password.value.charAt(password.value.lenght -i-1)) {
+			alert("Something is wrong!");
+			return;
+		}	 
+		}
+	alert("Well Done!!!");
+	
+	
+	 
   if(password.value != confirm_password.value) {
-        alert("Letters not same!");
+        alert("Letters are not the same!");
     } else {
-        alert("Letters same!");
+        alert("Letters are the same!");
   }
 }
+
